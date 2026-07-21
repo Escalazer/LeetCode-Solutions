38e8e8b35e6f5d68ac7be8267f622f13752ead24
Escalazer <225735655+Escalazer@users.noreply.github.com>
2026-07-22 01:27:32 +0530

    Time: 0 ms (100%), Space: 80.1 MB (78.27%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -1,12 +1,19 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
         int l = 0;
-        int h = 1;
+        int h = arr.length - 1;
+        int mid = 0;
 
-        while (arr[l] < arr[h] && h != arr.length) {
-            l++;
-            h++;
+        while (l < h) {
+            mid = (l + h) / 2;
+
+            if (arr[mid] < arr[mid + 1]) {
+                l = mid + 1;
+            }
+            else if (arr[mid] >= arr[mid + 1]) {
+                h = mid;
+            }
         }
-        return l;
+        return h;
     }
 }
