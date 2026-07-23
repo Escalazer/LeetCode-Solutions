64ab81ba4f318a83023aae1392bcb9f74147a6f7
Escalazer <225735655+Escalazer@users.noreply.github.com>
2026-07-23 16:15:49 +0530

    Time: 0 ms (100%), Space: 43.8 MB (46.77%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int l = 0;
+        int h = n - 1;
+        int guess = 0;
+
+        while (l <= h) {
+            guess = (l + h) / 2;
+
+            if (nums[guess] == target) return guess;
+
+            else if (nums[guess] > nums[n - 1]) {
+                if (nums[guess] < target) l = guess + 1;
+                else {
+                    if (nums[0] > target) l = guess + 1;
+                    else h = guess - 1;
+                }
+            }
+
+            else {
+                if (nums[guess] > target) h = guess - 1;
+                else {
+                    if (nums[n - 1] < target) h = guess - 1;
+                    else l = guess + 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
