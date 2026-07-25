6168da9154f52f8624f11eeeb9b8bdad9ffa2d16
Escalazer <225735655+Escalazer@users.noreply.github.com>
2026-07-25 14:44:52 +0530

    Time: 0 ms (100%), Space: 43.7 MB (80.22%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,17 +1,33 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int l = 0;
-        int h = (m * n) - 1;
+        int l = 0, h = matrix.length - 1;
         int guess = 0;
+        int row = 0;
+        
+        //finding the row number
+        while (l <= h) {
+            guess = (l + h) / 2;
+            
+            if (matrix[guess][0] == target) return true;
 
+            else if (matrix[guess][0] > target) h = guess - 1;
+
+            else {
+                row = guess;
+                l = guess + 1;
+            }
+        }
+
+        l = 0; h = matrix[0].length - 1;
+        guess = 0;
+
+        //finding the column number
         while (l <= h) {
             guess = (l + h) / 2;
 
-            if (matrix[guess / n][guess % n] == target) return true;
+            if (matrix[row][guess] == target) return true;
 
-            else if (matrix[guess / n][guess % n] > target) h = guess - 1;
+            else if (matrix[row][guess] > target) h = guess - 1;
 
             else l = guess + 1;
         }
