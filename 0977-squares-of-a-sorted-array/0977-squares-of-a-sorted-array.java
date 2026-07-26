d315b5fedbdf09181f00befbd2f09a9d6e64fd3c
Escalazer <225735655+Escalazer@users.noreply.github.com>
2026-07-26 16:13:17 +0530

    Time: 1 ms (100%), Space: 47 MB (94.85%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,58 +1,23 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        ArrayList<Integer> neg = new ArrayList<>();
-        ArrayList<Integer> pos = new ArrayList<>();
-        
-        for (int num : nums) {
-            if (num >= 0) pos.add(num);
-            else neg.add(num);
-        }
+        int i = 0;
+        int j = nums.length - 1;
+        int[] res = new int[nums.length];
+        int id = res.length - 1;;
 
-        if (neg.size() == 0) {
-            for (int i = 0; i < pos.size(); i++) {
-                pos.set(i, pos.get(i)*pos.get(i));
+        while (i <= j) {
+            int s1 = nums[i]*nums[i];
+            int s2 = nums[j]*nums[j];
+
+            if (s1 > s2) {
+                res[id--] = s1;
+                i++;
             }
-            int[] posarr = new int[pos.size()];
-            for (int i = 0; i < pos.size(); i++) {
-                posarr[i] = pos.get(i);
+            else {
+                res[id--] = s2;
+                j--;
             }
-            return posarr;
         }
-
-        if (pos.size() == 0) {
-            for (int i = 0; i < neg.size(); i++) {
-                neg.set(i, neg.get(i)*neg.get(i));
-            }
-            Collections.reverse(neg);
-            int[] negarr = new int[neg.size()];
-            for (int i = 0; i < neg.size(); i++) {
-                negarr[i] = neg.get(i);
-            }
-            return negarr;
-        }
-
-        int i = 0, j = 0, id = 0;
-        int m = pos.size();
-        int n = neg.size();
-        int[] res = new int[m + n];
-
-        for (i = 0; i < pos.size(); i++) {
-            pos.set(i, pos.get(i)*pos.get(i));
-        }
-        for (i = 0; i < neg.size(); i++) {
-            neg.set(i, neg.get(i)*neg.get(i));
-        }
-        Collections.reverse(neg);
-
-        i = 0;
-        while (i < m && j < n) {
-            if (pos.get(i) <= neg.get(j)) res[id++] = pos.get(i++);
-            else res[id++] = neg.get(j++);
-        }
-        while (i < m) res[id++] = pos.get(i++);
-
-        while (j < n) res[id++] = neg.get(j++);
-
         return res;
     }
 }
