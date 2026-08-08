a3341e2d00d6c4934dc05b55439c299400960d28
Escalazer <225735655+Escalazer@users.noreply.github.com>
2026-08-08 21:34:10 +0530

    Time: 14 ms (22.33%), Space: 47.7 MB (14.43%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,22 +1,20 @@
 class Solution {
-    int lengthOfLongestSubstring(String s) {
+    public int lengthOfLongestSubstring(String s) {
+        int l = 0;
+        int maxlen = 0;
+        int[] freq = new int[128];
 
-        Set<Character> charSet = new HashSet<>();
+        for (int r = 0; r < s.length(); r++) {
+            freq[s.charAt(r)]++;
 
-        int maxLength = 0;
-        int left = 0;
-
-        for (int right = 0; right < s.length(); right++) {
-
-            while (charSet.contains(s.charAt(right))) {
-                charSet.remove(s.charAt(left));
-                left++;
+            while (freq[s.charAt(r)] > 1) {
+                freq[s.charAt(l)]--;
+                l++;
             }
 
-            charSet.add(s.charAt(right));
-            maxLength = Math.max(maxLength, right - left + 1);
+            maxlen = Math.max(maxlen, r - l + 1);
         }
 
-        return maxLength;
+        return maxlen;
     }
 }
