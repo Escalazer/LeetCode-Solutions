f8b853cc966cea028b443d66dc7db62e9c56a80a
Escalazer <225735655+Escalazer@users.noreply.github.com>
2026-08-10 11:40:55 +0530

    Time: 49 ms (57.25%), Space: 50.1 MB (69.25%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -1,45 +1,29 @@
 class Solution {
     public int subarraysWithKDistinct(int[] nums, int k) {
-        int l1 = 0;
-        int l2 = 0;
+        return atmost(nums, k) - atmost(nums, (k - 1));
+    }
+
+    public int atmost(int[] nums, int k) {
+        int l = 0;
         int r = 0;
-        int count1 = 0;
-        int count2 = 0;
-        HashMap<Integer, Integer> map1 = new HashMap<>();
-        HashMap<Integer, Integer> map2 = new HashMap<>();
+        int count = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
 
         for (r = 0; r < nums.length; r++) {
             //map1 is to store frequency of each integer
             //count1 is to count no of subarrays with <= k distinct numbers
-            map1.put(nums[r], map1.getOrDefault(nums[r], 0) + 1);
+            map.put(nums[r], map.getOrDefault(nums[r], 0) + 1);
 
-            while (map1.size() > k) {
-                map1.put(nums[l1], map1.get(nums[l1]) - 1);
-                if (map1.get(nums[l1]) == 0) {
-                    map1.remove(nums[l1]);
+            while (map.size() > k) {
+                map.put(nums[l], map.get(nums[l]) - 1);
+                if (map.get(nums[l]) == 0) {
+                    map.remove(nums[l]);
                 }
-                l1++;
+                l++;
             }
 
-            if (map1.size() <= k) {
-                count1 += r - l1 + 1;
-            }
-
-            //map2 is to store frequency of each integer
-            //count2 is to count no of subarrays with < k distinct numbers
-            map2.put(nums[r], map2.getOrDefault(nums[r], 0) + 1);
-
-             while (map2.size() >= k) {
-                map2.put(nums[l2], map2.get(nums[l2]) - 1);
-                if (map2.get(nums[l2]) == 0) {
-                    map2.remove(nums[l2]);
-                }
-                l2++;
-            }
-            if (map2.size() < k) {
-                count2 += r - l2+ 1;
-            }
+            count += r - l + 1;
         }
-        return (count1 - count2);
+        return count;
     }
 }
