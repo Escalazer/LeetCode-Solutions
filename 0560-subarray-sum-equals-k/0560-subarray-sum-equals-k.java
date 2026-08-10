71e5c15ba3485a307cf8047553a1434c4e309d13
Escalazer <225735655+Escalazer@users.noreply.github.com>
2026-08-10 09:28:46 +0530

    Time: 25 ms (46.56%), Space: 48.6 MB (70.36%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, 1);
+
+        int prefixsum = 0, count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            prefixsum += nums[i];
+            int check = prefixsum - k;
+            count += map.getOrDefault(check, 0);
+            map.put(prefixsum, map.getOrDefault(prefixsum, 0) + 1);
+        }
+        return count;
+    }
+}
