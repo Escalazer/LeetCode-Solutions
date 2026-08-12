bc4d556eda3020e1f6912b9ed4c5db8acaeb1dc0
Escalazer <225735655+Escalazer@users.noreply.github.com>
2026-08-12 20:30:15 +0530

    Time: 15 ms (18.94%), Space: 46.7 MB (15.75%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String a = s.toLowerCase().replaceAll("[^A-Za-z0-9]", "");
+        s = s.toLowerCase().replaceAll("[^A-Za-z0-9]", "");
         int i = 0;
-        int j = a.length() - 1;
+        int j = s.length() - 1;
 
         while (i < j) {
-            if (a.charAt(i) != a.charAt(j)) return false;
+            if (s.charAt(i) != s.charAt(j)) return false;
             i++;
             j--;
         }
