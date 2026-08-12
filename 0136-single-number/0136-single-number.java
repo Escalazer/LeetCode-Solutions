446936dfc4e51ff00a100f1de9ee8a64f956d91d
Escalazer <225735655+Escalazer@users.noreply.github.com>
2026-08-12 21:01:57 +0530

    Time: 1 ms (99.94%), Space: 47.2 MB (13.87%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int value = 0;
+        for (int num : nums) {
+            value = value ^ num;
+        }
+        return value;
+    }
+}
