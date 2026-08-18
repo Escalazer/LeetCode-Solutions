9dfd85b35cbbdaabd8579f9d9abac8a5f59b20f0
Escalazer <225735655+Escalazer@users.noreply.github.com>
2026-08-18 19:33:27 +0530

    Time: 29 ms (93.37%), Space: 60.6 MB (5.53%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -6,6 +6,7 @@ class Solution {
 
         for (int i = 0; i < n - 2; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) continue;
+            if (nums[i] > 0) break;
             int j = i + 1;
             int k = n - 1;
             while (j < k) {
