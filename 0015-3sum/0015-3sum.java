2197d6325fe3229d550cb108aa43353c752cd78e
Escalazer <225735655+Escalazer@users.noreply.github.com>
2026-08-18 20:02:38 +0530

    Time: 29 ms (93.37%), Space: 59.9 MB (16.47%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -4,7 +4,7 @@ class Solution {
         Arrays.sort(nums);
         List<List<Integer>> ans = new ArrayList<>();
 
-        for (int i = 0; i < n - 2; i++) {
+        for (int i = 0; i < n; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) continue;
             if (nums[i] > 0) break;
             int j = i + 1;
