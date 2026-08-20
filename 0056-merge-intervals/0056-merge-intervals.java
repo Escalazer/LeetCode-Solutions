9eacb3802c2ad16c1530d658e9f899e21f80a766
Escalazer <225735655+Escalazer@users.noreply.github.com>
2026-08-20 14:08:54 +0530

    Time: 8 ms (91.66%), Space: 49.2 MB (43.2%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -2,7 +2,7 @@ class Solution {
     public int[][] merge(int[][] intervals) {
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
 
-        List<List<Integer>> l = new ArrayList<>();
+        List<int[]> ans = new ArrayList<>();
 
         int start1 = intervals[0][0];
         int end1 = intervals[0][1];
@@ -14,25 +14,13 @@ class Solution {
                 end1 = Math.max(end1, end2);
             }
             else {
-                List<Integer> temp = new ArrayList<>();
-                temp.add(start1);
-                temp.add(end1);
-                l.add(temp);
+                ans.add(new int[]{start1, end1});
                 start1 = start2;
                 end1 = end2;
             }
         }
-        List<Integer> temp = new ArrayList<>();
-        temp.add(start1);
-        temp.add(end1);
-        l.add(temp);
+        ans.add(new int[]{start1, end1});
 
-        int ans[][] = new int[l.size()][2];
-        for (int i = 0; i < ans.length; i++) {
-            ans[i][0] = l.get(i).get(0);
-            ans[i][1] = l.get(i).get(1);
-        }
-
-        return ans;
+        return ans.toArray(new int[ans.size()][]);
     }
 }
