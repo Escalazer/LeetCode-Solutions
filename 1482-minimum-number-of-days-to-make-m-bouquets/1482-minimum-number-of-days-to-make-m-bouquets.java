273ece83a588fd40b6bfe465ea7eef81cfdfac88
Escalazer <225735655+Escalazer@users.noreply.github.com>
2026-08-21 15:32:15 +0530

    Time: 29 ms (5.27%), Space: 82.6 MB (67.49%) - LeetHub

---
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean possible(int[] arr, int mid, int m, int k) {
+        int n = arr.length;
+        int count = 0;
+        int no_of_bouquets = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] <= mid) {
+                count++;
+            }
+            else {
+                no_of_bouquets += count / k;
+                count = 0;
+            }
+        }
+        no_of_bouquets += count / k;
+        if (no_of_bouquets >= m)
+            return true;
+        else
+            return false;
+    }
+
+    public int minDays(int[] bloomDay, int m, int k) {
+        if (bloomDay.length < (m * k)) return -1;
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MIN_VALUE;
+        for (int bloom : bloomDay) {
+            max = Math.max(max, bloom);
+            min = Math.min(min, bloom);
+        }
+        int l = min, r = max, mid = -1;
+        while (l <= r) {
+            mid = (l + r) / 2;
+            boolean poss = possible(bloomDay, mid, m, k);
+            if (poss == true)
+                r = mid - 1;
+            else
+                l = mid + 1;
+        }
+        if (possible(bloomDay, l, m, k))
+            return l;
+        else
+            return -1;
+    }
+}
