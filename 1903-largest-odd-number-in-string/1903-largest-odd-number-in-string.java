4c72dfcf3602ab6d5238b0f36b3a640f60843f49
Escalazer <225735655+Escalazer@users.noreply.github.com>
2026-08-24 16:15:50 +0530

    Time: 1 ms (99.89%), Space: 47.1 MB (56.23%) - LeetHub

---
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String largestOddNumber(String num) {
+        int n = num.length() - 1;
+        while (n >= 0) {
+            if (num.charAt(n) % 2 == 1)
+                return num.substring(0, n + 1);
+            n--;
+        }
+        return "";
+    }
+}
