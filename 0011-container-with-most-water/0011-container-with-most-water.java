f19cb388995c5c9a5d6b49a3980ba355704ec02c
Escalazer <225735655+Escalazer@users.noreply.github.com>
2026-08-27 02:39:56 +0530

    Time: 5 ms (83.11%), Space: 77.3 MB (69.01%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,13 +1,11 @@
 class Solution {
     public int maxArea(int[] height) {
-        int water = 0;
         int maxwater = 0;
         int l = 0;
         int r = height.length - 1;
 
         while (l < r) {
-            water = Math.min(height[l], height[r]) * (r - l);
-            maxwater = Math.max(maxwater, water);
+            maxwater = Math.max(maxwater, (Math.min(height[l], height[r]) * (r - l)));
 
             if (height[l] < height[r])
                 l++;
