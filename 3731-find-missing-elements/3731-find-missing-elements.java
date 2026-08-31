611835bc1660f06f02da25a43a0171d365d71270
Escalazer <225735655+Escalazer@users.noreply.github.com>
2026-09-01 01:00:13 +0530

    Time: 7 ms (20.25%), Space: 47.3 MB (21.05%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.java
+++ 3731-find-missing-elements/3731-find-missing-elements.java
@@ -1,17 +1,15 @@
 class Solution {
     public List<Integer> findMissingElements(int[] nums) {
         Arrays.sort(nums);
+
         List<Integer> list = new ArrayList<>();
+
         for (int i = 1; i < nums.length; i++) {
-            if (nums[i] - nums[i - 1] > 1) {
-                int j = nums[i] - nums[i - 1];
-                j--; // ex:- [0,1,2,5,6] j = 5 - 2 = 3. j-- = 2. 5 - 2 = 3. 5 - 1 = 4.
-                while (j > 0) {
-                    list.add(nums[i] - j);
-                    j--;
-                }
+            for (int j = nums[i - 1] + 1; j < nums[i]; j++) {
+                list.add(j);
             }
         }
+
         return list;
     }
 }
