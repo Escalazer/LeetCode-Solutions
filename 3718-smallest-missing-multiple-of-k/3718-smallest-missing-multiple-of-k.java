52ecf7181092d406316b4f458054326cf3a61130
Escalazer <225735655+Escalazer@users.noreply.github.com>
2026-09-01 00:20:35 +0530

    Time: 1 ms (88.73%), Space: 45.2 MB (66.48%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
@@ -1,16 +1,14 @@
 class Solution {
     public int missingMultiple(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+        HashSet<Integer> set = new HashSet<>();
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] % k == 0)
-                map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+                set.add(nums[i]);
         }
-        int factor = 1;
-        while(true) {
-            int potential_ans = k * factor;
-            if (map.containsKey(potential_ans) == false)
-                return potential_ans;
-            factor++;
+        int potential_ans = k;
+        while(set.contains(potential_ans)) {
+            potential_ans += k;
         }
+        return potential_ans;
     }
 }
