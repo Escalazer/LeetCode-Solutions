665a77f973d67b1adc0245d5afc77387414a0ab0
Escalazer <225735655+Escalazer@users.noreply.github.com>
2026-09-06 23:55:59 +0530

    Time: 0 ms (100%), Space: 48.2 MB (55.28%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,17 +1,13 @@
 class Solution {
-    public void swap (char[] s, int l ,int r) {
-        if (l >= r) return;
-        char temp = s[l];
-        s[l] = s[r];
-        s[r] = temp;
-        l++;
-        r--;
-        swap (s,l,r);
-    }
     public void reverseString(char[] s) {
         int n = s.length;
-        int l = 0;
-        int r = n - 1;
-        swap (s, l, r);
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            char temp = s[l];
+            s[l] = s[r];
+            s[r] = temp;
+            l++;
+            r--;
+        }
     }
 }
