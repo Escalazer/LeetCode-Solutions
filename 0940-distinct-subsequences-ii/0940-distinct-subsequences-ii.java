def4ef1de7e70337fce1343b9bf5deef5051290a
Escalazer <225735655+Escalazer@users.noreply.github.com>
2026-09-07 22:12:59 +0530

    Time: 2 ms (100%), Space: 43.4 MB (41.84%) - LeetHub

---
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.java
@@ -0,0 +1,23 @@
+class Solution { //done to maintain streak
+    public int distinctSubseqII(String s) {
+
+        int MOD = 1_000_000_007;
+
+        long dp = 1;  // empty subsequence
+
+        long[] last = new long[26];
+
+        for (char c : s.toCharArray()) {
+
+            int idx = c - 'a';
+
+            long newDp = (2 * dp - last[idx] + MOD) % MOD;
+
+            last[idx] = dp;
+
+            dp = newDp;
+        }
+
+        return (int)((dp - 1 + MOD) % MOD);
+    }
+}
