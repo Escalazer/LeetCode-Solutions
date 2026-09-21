f62fd09c368b701b451a23ab523fcafdd10b5af1
Escalazer <225735655+Escalazer@users.noreply.github.com>
2026-09-21 19:23:26 +0530

    Time: 1 ms (100%), Space: 44 MB (50.3%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -1,9 +1,9 @@
 class Solution {
     public int reverseDegree(String s) {
-        int ans = 0;
-        for (int i = 0; i < s.length(); i++)
+        int ans = 0, i;
+        for (i = 0; i < s.length(); i++)
             ans += (i + 1) * (122 - s.charAt(i) + 1);
-            
+
         return ans;
     }
 }
